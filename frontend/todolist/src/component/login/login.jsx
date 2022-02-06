10c221ca607a35b9c8b95cabd7d87802d2c17125
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-06 12:12:44 +0900

    feature/firestSet: ADD/ loginUI

--- frontend/todolist/src/component/login/login.jsx
+++ frontend/todolist/src/component/login/login.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import styles from "./login.module.css"
 const Login = (props) => {
   return (
     <section className={styles.login}>
       <form className={styles.mailLogin}>
-        <input type='text' className={styles.id} />
-        <input type='text' className={styles.password} />
+        <input type='text' className={styles.id} placeholder="  ID"/>
+        <input type='text' className={styles.password} placeholder="  PASSWORD"/>
       </form>
-      <div clasName={styles.ssoLogin}>
+      <div className={styles.ssoLogin}>
         <button className={styles.googleBtn}>Google</button>
         <button className={styles.githubBtn}>GitHub</button>
       </div>
