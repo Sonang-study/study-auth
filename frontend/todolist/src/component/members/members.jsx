fc55440b6d94922446fdfc92728f6474203f6a97
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-06 13:54:42 +0900

    feature/firestSet: ADD / membersUI

--- frontend/todolist/src/component/members/members.jsx
+++ frontend/todolist/src/component/members/members.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import styles from './members.module.css';
+import Member from '../member/member';
 
 const Members = (props) => {
-  return <span>member</span>;
+  const members = [
+    { name: 'Sonang' },
+    { name: '405' },
+    { name: 'DongDaemoon' },
+    { name: 'What' },
+    { name: 'SHINKO' },
+    { name: 'Sonang' },
+    { name: '405' },
+    { name: 'DongDaemoon' },
+  ];
+  return (
+    <section className={styles.members}>
+      <section className={styles.btns}>
+        <FontAwesomeIcon icon={faPlus} className={styles.plusBtn} />
+      </section>
+      <section className={styles.member}>
+        {members.map((member) => (
+          <Member member={member} />
+        ))}
+      </section>
+    </section>
+  );
 };
 
 export default Members;
