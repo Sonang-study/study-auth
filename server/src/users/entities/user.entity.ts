cd650c01bf6f6f3e1e05a105b428276b7bf9c787
고준영 <jayko95@naver.com>
2022-02-12 15:03:23 +0900

    feat(user): Swagger 보충
    swagger 내용 보충

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -4,6 +4,7 @@ import { Entity, Column, OneToMany, BeforeInsert } from 'typeorm'
 import { Task } from '../../tasks/entities/task.entity'
 import * as bcrypt from 'bcrypt'
 import { InternalServerErrorException } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 enum SignupMethod {
   local = 'local',
@@ -12,22 +13,44 @@ enum SignupMethod {
 
 @Entity()
 export class User extends CoreEntity {
+  @ApiProperty({
+    example: 'jay',
+    description: 'user first name',
+  })
   @Column()
   @IsString()
   firstName: string;
 
+  @ApiProperty({
+    example: 'ko',
+    description: 'user last name',
+  })
   @Column()
   @IsString()
   lastName: string;
 
+  @ApiProperty({
+    example: 'jayko@naver.com',
+    description: 'user email for use login ID',
+    uniqueItems: true,
+  })
   @Column({ unique: true })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: '1q2w3e4r!',
+    description: 'user password for login',
+  })
   @Column({ select: false })
   @IsString()
   password: string;
 
+  @ApiProperty({
+    example: 'local',
+    description: 'user signup method',
+    default: 'local',
+  })
   @Column({default: SignupMethod.local , type:'enum' ,enum:SignupMethod})
   @IsOptional()
   @IsString()
