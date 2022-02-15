eef66dae11e133ea1224b13bad0c310538363932
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-15 21:59:21 +0900

    feature/firestSet: ADD / EditUI, ADD Group, member UI

--- frontend/todolist/src/component/members/members.jsx
+++ frontend/todolist/src/component/members/members.jsx
@@ -4,7 +4,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './members.module.css';
 import Member from '../member/member';
 
-const Members = (props) => {
+const Members = ({ onPopupClick }) => {
   const members = [
     { name: 'Sonang' },
     { name: '405' },
@@ -15,10 +15,19 @@ const Members = (props) => {
     { name: '405' },
     { name: 'DongDaemoon' },
   ];
+
+  const onAddMember = () => {
+    onPopupClick();
+  };
+
   return (
     <section className={styles.members}>
       <section className={styles.btns}>
-        <FontAwesomeIcon icon={faPlus} className={styles.plusBtn} />
+        <FontAwesomeIcon
+          icon={faPlus}
+          className={styles.plusBtn}
+          onClick={onAddMember}
+        />
       </section>
       <section className={styles.member}>
         {members.map((member) => (
