87919febe09c7908c0f64e75431805c6890d211f
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-02-20 23:56:08 +0900

    ADD / dummy data

---
+++ frontend/todolist/src/db/database.js
@@ -0,0 +1,32 @@
+const tasks = {
+  id: 1,
+  createdAt: '2022-02-13T12:08:54.850Z',
+  updatedAt: '2022-02-13T12:41:32.000Z',
+  date: '2022-02-12T03:00:00.000Z',
+  plan: 'testplan',
+  image: 'image.png',
+  taskDays: [
+    {
+      id: 1,
+      createdAt: '2022-02-13T12:43:28.511Z',
+      updatedAt: '2022-02-13T12:45:13.000Z',
+      finishedAt: '2022-02-12 12:00:00',
+      dayPlan: 'testdayplan',
+    },
+    {
+      id: 3,
+      createdAt: '2022-02-13T13:04:18.003Z',
+      updatedAt: '2022-02-13T13:04:18.003Z',
+      finishedAt: null,
+      dayPlan: 'testdayplan',
+    },
+    {
+      id: 4,
+      createdAt: '2022-02-13T13:07:03.601Z',
+      updatedAt: '2022-02-13T13:07:03.601Z',
+      finishedAt: null,
+      dayPlan: 'testdayplan',
+    },
+  ],
+};
+export default tasks;
