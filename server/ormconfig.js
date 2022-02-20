cfcfc8d72d2f4b80e618ad50754b65fe53417ad0
고준영 <jayko95@naver.com>
2022-02-20 18:12:09 +0900

    feat(DevOps): DB 환경변수 수정

---
+++ server/ormconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "type": "mysql",
+  "host": process.env.DB_HOST || "localhost",
+  "port": process.env.DB_PORT || 3306,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": "study-auth",
+  "synchronize": true,
+  "logging": process.env.NODE_ENV === 'prod' ? false : true,
+  "entities": ["dist/**/*/entities/*.entity{.ts,.js}"]
+}
