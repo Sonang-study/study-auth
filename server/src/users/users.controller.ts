47598b37520e0f36256385b5eb56a02fc5790b4c
고준영 <jayko95@naver.com>
2022-02-26 12:17:35 +0900

    feat(Add_Auth): JWT Auth Guard 위치 변경

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -7,34 +7,30 @@ import { UpdateUserDto } from './dtos/updateUser.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
-
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @ApiOperation({summary:'사용자 리스트'})
-  @UseGuards(JwtAuthGuard)
   @Get()
   async getUserList(){
     return await this.usersService.getAll();
   }
 
   @ApiOperation({summary:'사용자 상세정보'})
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getUser(@Param('id', ParseIntPipe) id:number) {
     return await this.usersService.getOne(id);
   }
 
   @ApiOperation({summary:'사용자 정보 수정'})
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async updateUser(@Param('id', ParseIntPipe) id:number, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.updateOne(id, updateUserDto);
   }
 
   @ApiOperation({summary:'회원탈퇴'})
-  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async deleteUser(@Param('id', ParseIntPipe) id:number) {
     return await this.usersService.getOne(id);
