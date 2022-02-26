4bcf0ef0dbcc612edc78e22136ce106141417d8e
고준영 <jayko95@naver.com>
2022-02-26 12:11:37 +0900

    feat(Add_Auth): 회원가입 endpoint 수정

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { CreateUserDto } from './users/dtos/createUser.dto';
 import { LoginUserDto } from './users/dtos/loginUser.dto.ts';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService:AuthService){}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    ){}
 
   @ApiBody({ type: [LoginUserDto] })
   @ApiOperation({ summary:'로그인' })
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    return this.authService.responseJWT(req.user);
+  }
+  @ApiBody({ type: [CreateUserDto] })
+  @ApiOperation({summary:'회원가입'})
+  @Post('auth/siginup')
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
   }
 }
