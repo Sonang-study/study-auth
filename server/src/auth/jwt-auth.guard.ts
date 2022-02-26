f2941871a36180d07956b45eab9a1f8489adc7fd
고준영 <jayko95@naver.com>
2022-02-26 12:15:02 +0900

    feat(Add_Auth): JWT Auth 기능 추가

--- server/src/auth/jwt-auth.guard.ts
+++ server/src/auth/jwt-auth.guard.ts
@@ -3,3 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
+/*
+AuthGuard는 Strategy를 자동으로 실행
+Strategy는 Validate 실행
+*/
--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,18 +1,32 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UnAuthorizedError } from 'src/common/errors/unAuthorized.error';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(@InjectRepository(User)
+    private readonly users: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.SECRET_KEY,
     });
   }
-
+/*
+Strategy실행되면 자동으로 validate 실행되기에 함수이름 변경하면 안됨
+*/
   async validate(payload: any) {
-    return { id: payload.id, email: payload.email };
+    const email = payload.email;
+    const id = payload.id;
+    const user = await this.users.findOne({email});
+
+    if (!user) throw new UnAuthorizedError();
+
+    return { id, email };
   }
 }
