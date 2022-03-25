e2c5359b63209f3fb836651c73b2382d05bb5fa0
고준영 <jayko95@naver.com>
2022-03-25 13:38:51 +0900

    feat(groupModeling): 그룹 서비스 추가

--- server/src/group/group.module.ts
+++ server/src/group/group.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { SController } from './s/s.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Group } from './entities/group.entity';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 
 @Module({
-  controllers: [SController, GroupController],
+  imports: [TypeOrmModule.forFeature([Group, User]),],
+  controllers: [GroupController],
   providers: [GroupService]
 })
 export class GroupModule {}
--- server/src/group/group.service.ts
+++ server/src/group/group.service.ts
@@ -1,4 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AlreadyExistError } from 'src/common/errors/already-exist.error';
+import { UnAuthorizedError } from 'src/common/errors/unAuthorized.error';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Group } from './entities/group.entity';
 
 @Injectable()
-export class GroupService {}
+export class GroupService {
+  constructor(
+    @InjectRepository(Group) private readonly groups: Repository<Group>,
+    @InjectRepository(User) private readonly users: Repository<User>,
+  ) {}
+
+  async createGroup({name}, user): Promise<string> {
+    const exist = await this.groups.findOne({where: { name }})
+    const userObj = await this.users.findOne({where: { id: user.id }})
+
+    if (exist) throw new AlreadyExistError();
+
+    const newgroup = this.groups.create({name});
+    newgroup.masterUserId = user.id
+    newgroup.users = [userObj];
+
+    await this.groups.save(newgroup);
+
+    return "success create group";
+  }
+
+  async inviteUser(groupId, userId): Promise<string> {
+    const user = await this.users.findOne({where: {id: userId}})
+    const group = await this.groups.findOne({
+      where: {id: groupId},
+      relations: ['users']
+    });
+
+    group.users = [user, ...group.users];
+    await this.groups.save(group);
+
+    return "Sucess invite"
+  }
+
+  async updateGroup({name}, groupId, user): Promise<string> {
+    const group = await this.groups.findOne({where: { id: groupId }})
+
+    if(!group.masterUserId === user.id) throw new UnAuthorizedError();
+
+    group.name = name;
+    await this.groups.save(group);
+
+    return "Sucess update";
+  }
+
+  async getMyGroup(user): Promise<Group[]> {
+    const userObj = await this.users.findOne({
+      where: {id: user.id},
+      relations: ['groups'],
+    })
+    return userObj.groups;
+  }
+
+  async getGroupMembers(groupId): Promise<User[]> {
+    const group = await this.groups.findOne({
+      where: { id: groupId },
+      relations: ['users'],
+    })
+
+    return group.users;
+  }
+
+  async deleteGroup(groupId, user): Promise<string> {
+    const group = await this.groups.findOne({where: { id: groupId }})
+
+    if(!group.masterUserId === user.id) throw new UnAuthorizedError();
+
+    await this.groups.delete(group);
+
+    return "Sucess delete";
+  }
+}
